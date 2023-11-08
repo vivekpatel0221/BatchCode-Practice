51872b9a2de74053f9a9b700913837c26e9aee74
chintu <chintu@192.168.100.13>
2023-11-08 22:53:53 +0530

    commited

--- src/test/java/demo/work1demo.java
+++ src/test/java/demo/work1demo.java
@@ -4,7 +4,7 @@ public class work1demo {
 	public static void main(String[] args) {
 		System.out.println("vivek");
 		
-		
+		System.out.println("conflict");
 		
 		System.out.println("changess");
 		
